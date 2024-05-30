fef6064743c6d4d223db9d4beb36fa35023734f3
PereraAPD <161456406+PereraAPD@users.noreply.github.com>
2024-05-30 09:51:04 +0530

    deleted first version

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
-
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{user}', [UserController::class, 'show']);
