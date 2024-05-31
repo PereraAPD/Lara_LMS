96130f3aa4809af12bc5f07cf6a64b22e8aedb11
PereraAPD <161456406+PereraAPD@users.noreply.github.com>
2024-05-31 14:18:24 +0530

    new files added

---
+++ config/loan_schemes.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'General' => [
+        ['name' => 'General', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Sonduru Piyasa' => [
+        ['name' => 'Sonduru Piyasa', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    // Add more loan schemes as needed
+];
---
+++ resources/views/loans/sihina-maligaya/partials/section1-info.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100">Section 1 Information</h3>
+    <p class="mt-2 text-gray-700 dark:text-gray-300">
+        <!-- Add section 1 information here -->
+    </p>
+</div>
