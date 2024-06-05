07993c7697680f99a7d8fb3f3b8059383ce861dc
PereraAPD <161456406+PereraAPD@users.noreply.github.com>
2024-06-05 10:02:19 +0530

    loan schemes config updated

--- config/loan_schemes.php
+++ config/loan_schemes.php
@@ -11,5 +11,84 @@ return [
         ['name' => 'Section 1', 'slug' => 'section1'],
         ['name' => 'Section 2', 'slug' => 'section2'],
     ],
-    // Add more loan schemes as needed
+    'Govi Navoda' => [
+        ['name' => 'Govi Navoda', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Riya Shakthi' => [
+        ['name' => 'Riya Shakthi', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Ran Aswenna' => [
+        ['name' => 'Ran Aswenna', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Jaya Isura' => [
+        ['name' => 'Jaya Isura', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Rivi Bala Savi' => [
+        ['name' => 'Rivi Bala Savi', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Diri Saviya' => [
+        ['name' => 'Diri Saviya', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Madya Aruna' => [
+        ['name' => 'Madya Aruna', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Green Loan' => [
+        ['name' => 'Green Loan', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Home Sweet Home' => [
+        ['name' => 'Home Sweet Home', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Sihina Maligaya' => [
+        ['name' => 'Sihina Maligaya', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Rekawaranaya' => [
+        ['name' => 'Rekawaranaya', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Mini Taxi' => [
+        ['name' => 'Mini Taxi', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Sigithi Pasala' => [
+        ['name' => 'Sigithi Pasala', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'Working Capital' => [
+        ['name' => 'Working Capital', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'City Ride' => [
+        ['name' => 'City Ride', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
+    'My Future' => [
+        ['name' => 'My Future', 'slug' => 'main'],
+        ['name' => 'Section 1', 'slug' => 'section1'],
+        ['name' => 'Section 2', 'slug' => 'section2'],
+    ],
 ];
