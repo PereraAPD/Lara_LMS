7b212984753e912ffeca9cc385f69c8255a1685d
PereraAPD <161456406+PereraAPD@users.noreply.github.com>
2024-06-06 10:58:33 +0530

    period assignment functions added, minor issues

--- resources/views/loans/general/period-assign.blade.php
+++ resources/views/loans/general/period-assign.blade.php
@@ -9,10 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    @include('loans.general.partials.period-assign-form')
+                @include('loans.general.partials.period-assign-form')
                 </div>
             </div>
-            <!-- Add more sections or content as needed -->
+
+            <!-- Your Table -->
+            @include('loans.general.partials.period-assign-table')
         </div>
     </div>
 </x-app-layout>
+
+<script>
+function printRecords() {
+    // Fetch the content of the table
+    var tableContent = document.querySelector('.table-container').outerHTML;
+
+    // Create a new window for printing
+    var printWindow = window.open('', '_blank');
+
+    // Set the content of the new window to be the table content
+    printWindow.document.write('<html><head><title>Print Records</title></head><body>');
+    printWindow.document.write(tableContent);
+    printWindow.document.write('</body></html>');
+
+    // Close the document for printing
+    printWindow.document.close();
+
+    // Trigger the print function
+    printWindow.print();
+}
+</script>
